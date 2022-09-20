7f73f6af57ac2ed2ea1ab3ad94e7c91487f34b91
FelipeArredond <pipechiqui77@gmail.com>
2022-09-20 14:16:52 -0500

    Guayabal added

--- src/nodes.py
+++ src/nodes.py
@@ -13,3 +13,14 @@ trinidad['viva_laur'] = 4.5
 trinidad['santafe'] = 6.0
 trinidad['premium_plaza'] = 3.2
 trinidad['plaza_fabricato'] = 3.3
+
+guayabal['arkadia'] = 2.7
+guayabal['tesoro'] = 6.6
+guayabal['viva_env'] = 3.8
+guayabal['monterrey'] = 3.6
+guayabal['molinos'] = 5.9
+guayabal['oviedo'] = 2.9
+guayabal['viva_laur'] = 6.8
+guayabal['santafe'] = 2.6
+guayabal['premium_plaza'] = 5.9
+guayabal['plaza_fabricato'] = 5.9
